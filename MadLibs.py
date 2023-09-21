a41fb556a183528afed6bd35a9ea4efe3dcb4f0a
H4L1M4T0U <144480724+H4L1M4T0U@users.noreply.github.com>
2023-09-21 13:21:33 -0400

    gs,jhreklhgwejshglihwlroeiyu;oti

---
+++ MadLibs.py
@@ -0,0 +1,47 @@
+x = 3
+y = float(3)
+print(x,y) 
+values = [1,2.23,5,7,2,30,15]
+for i in values:
+    print(i)
+    print(values[0])
+    print(values[6])
+x = "this is a thing"
+y= x.split( )
+z = y[0]
+print(y)
+print(z)
+
+
+MathQ= input("what is one plus two. write your answer in words please.")
+if MathQ == "two":
+    print("correct")
+else:
+    print("incorrect")
+    
+
+print (MathQ)
+
+
+
+x = "Loser"
+print(f"HAHA {x}")
+
+
+temp= input("Whats your preffered temperature?Dont put the degree symbol, just type the number")
+
+temp = 75
+if temp > 68:
+    print("Thats..Uhm..thats pretty warm")
+if temp == 68:
+    print('Now thats a perfect tempurature. Not too hot, not too cold.')
+if temp < 68: 
+    print("hmm. Either you like winter, or your an artic animal.")
+
+print(temp)
+
+
+
+
+
+             
---
+++ challenges.py
@@ -0,0 +1,15 @@
+
+# CHALLENGE
+MathQ= input("what is one plus two. write your answer in words please.")
+if MathQ == "two":
+    print("correct")
+else:
+    print("incorrect")
+
+print (MathQ)
+
+x = "Loser"
+print(f"HAHA {x}")
+
+
+
