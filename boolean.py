634825b2a6e3bca4d78454b8ba6a4decf743d4d9
H4L1M4T0U <144480724+H4L1M4T0U@users.noreply.github.com>
2023-09-26 13:17:57 -0400

    update

---
+++ boolean.py
@@ -0,0 +1,29 @@
+
+
+
+
+
+
+MathQ= input("what is one plus two. write your answer in words please.")
+if MathQ == "two":
+    print("correct")
+else:
+    print("incorrect")
+    print("HAHA LOSER")
+    
+
+print (MathQ)
+
+
+
+temp= input("Whats your preffered temperature? Dont put the degree symbol, just type the number")
+
+temp = 75
+if temp > 68:
+    print("Thats..Uhm..thats pretty warm")
+if temp == 68:
+    print('Now thats a perfect tempurature. Not too hot, not too cold.')
+if temp < 68: 
+    print("hmm. Either you like winter, or your an artic animal.")
+
+print (temp)
--- challenges.py
+++ challenges.py
@@ -1,15 +1,31 @@
+#ILL PUT ALL THE CHALLENGES HER SO I DONT  GET CONFUSED!)
 
-# CHALLENGE
-MathQ= input("what is one plus two. write your answer in words please.")
-if MathQ == "two":
-    print("correct")
-else:
-    print("incorrect")
+#challenge 1
 
-print (MathQ)
+sent = input("please Type A Sentence ")
+print(sent) #so i can see that this is the variable value I expected
+broken = sent.split( )
 
-x = "Loser"
-print(f"HAHA {x}")
+print(len(broken))
+
+
+#m" Modulo" is what we use to create this function.(%) the number that is produced is the remainder
+#CHALLENGE NUMBER 2
+BigQ= input("TYPE A NUMBER AND I WILL DETERMINE IF ITS EVEN OR ODD")
+BigQ = input % 2
+if BigQ == 0 :
+    print("EVEN")
+elif BigQ == 1 :
+    print("ODD")
+
+BigQ(1)
+
+
+
+
+
+
+
 
 
 
