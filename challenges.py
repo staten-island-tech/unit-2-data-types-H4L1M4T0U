ab3e598c610663f8d110bb84c6767bf0f5010b6c
H4L1M4T0U <144480724+H4L1M4T0U@users.noreply.github.com>
2023-09-28 14:00:44 -0400

    645

--- challenges.py
+++ challenges.py
@@ -1,25 +1,63 @@
 #ILL PUT ALL THE CHALLENGES HER SO I DONT  GET CONFUSED!)
 
+#TIP CALCULATOR 
+def the_factors(x) :
+    print("these are  the factors of x :")
+    for i in range(1, x+1) :
+        if x % i == 0 :
+            print(i)
+the_factors(100)
+
+
+
 #challenge 1
 
+""" 
 sent = input("please Type A Sentence ")
 print(sent) #so i can see that this is the variable value I expected
 broken = sent.split( )
 
 print(len(broken))
-
+ """
 
 #m" Modulo" is what we use to create this function.(%) the number that is produced is the remainder
 #CHALLENGE NUMBER 2
-BigQ= input("TYPE A NUMBER AND I WILL DETERMINE IF ITS EVEN OR ODD")
-BigQ = input % 2
-if BigQ == 0 :
+BigQ= input ("TYPE A NUMBER AND I WILL DETERMINE IF ITS EVEN OR ODD")
+
+
+y = x % 2
+if y == 0:
     print("EVEN")
-elif BigQ == 1 :
+elif y > 0:
     print("ODD")
 
-BigQ(1)
 
+
+
+
+#CHALLENGE NUMBER 2
+Tip =input("Thank you for buying at totally not poisoned resturaunt.^^ How was ur service?(answer all lowercase. Choose between bad,okay,good,or great.)")
+if Tip == "bad" :
+ print(" 0% :(")
+if Tip == "okay" :
+   print("15% :c")
+if Tip == "good" :
+   print("20% c:")
+if Tip == "great" :
+   print("25% :D") 
+
+
+
+#challenge #3
+def the_factors(x) :
+    print("these are  the factors of x :")
+    for i in range(1, x+1) :
+        if x % i == 0 :
+            print(i)
+the_factors(100)
+
+
+   
 
 
 
