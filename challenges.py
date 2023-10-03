1575b3f56c722ed0e507573108fcea7b3c201c6c
H4L1M4T0U <144480724+H4L1M4T0U@users.noreply.github.com>
2023-10-03 13:41:55 -0400

    ALL 5 CHALLENGES DONE

--- challenges.py
+++ challenges.py
@@ -56,6 +56,18 @@ def the_factors(x) :
             print(i)
 the_factors(100)
 
+#Challenge #5
+def gcf(num1,num2) :
+   if num1 > num2 :
+      num1, num2 = num2, num1
+   for x in range (num1,0, -1) :
+      if num1 % x == 0 and num2 % x == 0:
+         return x
+num1 = 18
+num2 = 204
+print (str(gcf(num1, num2)))
+
+
 
    
 
